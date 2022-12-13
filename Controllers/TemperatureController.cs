fc9e8af560924ad15cdae674d91d2c0deeb6cc39
Anastasia Bashkirtseva <anastasia@bashkirtseva.com>
2022-12-13 14:18:40 +1100

    Update sensor URL

--- Controllers/TemperatureController.cs
+++ Controllers/TemperatureController.cs
@@ -14,7 +14,7 @@ namespace server.Controllers
         public async Task<IActionResult> Get(string id)
         {
             var http = new HttpClient();
-            var url = string.Format("https://temperature-sensor-service.herokuapp.com/sensor/{0}", id);
+            var url = string.Format("https://hasydbj5c4gpa2oozfpjpc677a0hxuob.lambda-url.ap-southeast-2.on.aws/sensor/{0}", id);
 
             var response = await http.GetAsync(url);
             var jsonString = await response.Content.ReadAsStringAsync();
